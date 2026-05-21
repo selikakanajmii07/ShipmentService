4e9c2f88388538f213fa6438be2287080408407d
selikakanajmii07 <selikakanajmii@upi.edu>
2026-05-21 07:15:20 +0700

    Update db.go

--- db.go
+++ db.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,7 +12,30 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "host=localhost port=5432 user=postgres password=Selikaknjm07 dbname=shipment_db sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "shipment_db"
+	}
+
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "postgres"
+	}
+
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "Selikaknjm07"
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
+		host, user, password, dbName,
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
